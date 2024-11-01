e784d22d4fea0911922ac7d4abcf7880289de0e4
mnnoh <dmnoh34795073@gmail.com>
2024-11-01 19:46:10 +0900

    나의 깃헙 코드

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title('나의 첫 스트림릿 웹앱')
+name = st.text_input('이름을 입력해주세요 : ')
+mbti = st.selectbox('MBTI를 선택해주세요', ['ENTJ','INTP'])
+
+if st.button('확인!') and name and mbti : 
+  print(name+'님은 정말 '+mbti+' 같아보이시네요!')
