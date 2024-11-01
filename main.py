77946d55bc25bc98bf9f57f0521c63aa76d9f948
mnnoh <dmnoh34795073@gmail.com>
2024-11-01 19:57:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ name = st.text_input('이름을 입력해주세요 : ')
 mbti = st.selectbox('MBTI를 선택해주세요', ['ENTJ','INTP'])
 
 if st.button('확인!') and name and mbti : 
-  print(name+'님은 정말 '+mbti+' 같아보이시네요!')
+  st.write(name+'님은 정말 '+mbti+' 같아보이시네요!')
